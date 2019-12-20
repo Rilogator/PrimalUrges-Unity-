11efcf2a26dc620a60405725a9c2444559d25dc5
Rilogator <jhowell713@gmail.com>
2019-12-20 12:32:29 -0500

    Basic attack sysem implimented
    Use scriptable objects in next update for damage calculation

--- Assets/PlayerCombat.cs
+++ Assets/PlayerCombat.cs
@@ -7,6 +7,12 @@ public class PlayerCombat : MonoBehaviour
 
     public Animator animator;
 
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+
     // Update is called once per frame
     void Update()
     {
@@ -20,7 +26,23 @@ public class PlayerCombat : MonoBehaviour
     {
         // Play attack animation
         animator.SetTrigger("Attack");
+
         // Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange, enemyLayers);
+
+
         // Damage them
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
