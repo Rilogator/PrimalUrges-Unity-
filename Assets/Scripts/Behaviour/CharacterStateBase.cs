ddf3d1f5b31bbd7fd341b6e6c4e2d33e5f14cddf
Rilogator <jhowell713@gmail.com>
2019-12-29 17:23:41 -0500

    Set up for using Animator as state machine

---
+++ Assets/Scripts/Behaviour/CharacterStateBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStateBase : StateMachineBehaviour
+{
+    // A reference to the character's controller
+    private CharacterController2D controller;
+
+    // Give this behaviour access to the controller script of our Character
+    public CharacterController2D GetCharacterController(Animator animator)
+    {
+        if (controller == null)
+            controller = animator.GetComponentInParent<CharacterController2D>();
+
+        return controller;
+    }
+}
