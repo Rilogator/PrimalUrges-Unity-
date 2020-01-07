992c2e52ad070356e9dc5a48c6eee8b54610cf42
Rilogator <jhowell713@gmail.com>
2020-01-07 12:36:26 -0500

    Added basic pathfinding, altered Player prefab (needs tweaks)

---
+++ Assets/Scripts/EnemyGFX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    [SerializeField] private AIPath aiPath = null;
+
+        // Update is called once per frame
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } 
+        else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+}
